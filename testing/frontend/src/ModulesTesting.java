51541a23e24a37bf10845c09aa557252153c9651
reverie <ss.saswatsahoo@gmail.com>
2018-03-10 07:49:42 +0530

    Renoved comments

--- testing/frontend/src/ModulesTesting.java
+++ testing/frontend/src/ModulesTesting.java
@@ -164,33 +164,33 @@ public class ModulesTesting {
 			//System.out.println(" - Social Login Modal " + (result_authentication.get(2) ? "Passed" : "Failed"));
 			
 			//Testing Initiative
-//			result_initiatives = moduleTesting.testInitiatives();
-//			
-//			System.out.println(" - Creating Initiative Modal " + (result_initiatives.get(0) ? "Passed" : "Failed"));
-//			System.out.println(" - Editing Initiative Modal " + (result_initiatives.get(1) ? "Passed" : "Failed"));
-//			System.out.println(" - Deleting Initiative Modal " + (result_initiatives.get(2) ? "Passed" : "Failed"));
-//
-//			//Testing Notifications
-//			result_notifications = moduleTesting.testNotifications();
-//			
-//			System.out.println(" - Header Notifications " + (result_notifications.get(0) ? "Passed" : "Failed"));
-//			System.out.println(" - Initiative Notifications " + (result_notifications.get(1) ? "Passed" : "Failed"));
-//	
-//			//Testing People
-//			result_people = moduleTesting.testPeople();
-//			
-//			System.out.println(" - Add People" + (result_people.get(0) ? "Passed" : "Failed"));
-//			System.out.println(" -  Delete People " + (result_notifications.get(1) ? "Passed" : "Failed"));
-//			
-//			//Testing Transfer
-//			result_transfer = moduleTesting.testTransfers();
-//			
-//			System.out.println(" - Transfering Tokens To User (Direct) " + (result_transfer.get(0) ? "Passed" : "Failed"));
-//			System.out.println(" - Transfering Tokens To User (Peer Reviewed) " + (result_transfer.get(1) ? "Passed" : "Failed"));
-//			
-//			result_profile = moduleTesting.testProfile();
-//			
-//			System.out.println(" - Editing Profile " + (result_profile.get(0) ? "Passed" : "Failed"));
+			result_initiatives = moduleTesting.testInitiatives();
+			
+			System.out.println(" - Creating Initiative Modal " + (result_initiatives.get(0) ? "Passed" : "Failed"));
+			System.out.println(" - Editing Initiative Modal " + (result_initiatives.get(1) ? "Passed" : "Failed"));
+			System.out.println(" - Deleting Initiative Modal " + (result_initiatives.get(2) ? "Passed" : "Failed"));
+
+			//Testing Notifications
+			result_notifications = moduleTesting.testNotifications();
+			
+			System.out.println(" - Header Notifications " + (result_notifications.get(0) ? "Passed" : "Failed"));
+			System.out.println(" - Initiative Notifications " + (result_notifications.get(1) ? "Passed" : "Failed"));
+	
+			//Testing People
+			result_people = moduleTesting.testPeople();
+			
+			System.out.println(" - Add People" + (result_people.get(0) ? "Passed" : "Failed"));
+			System.out.println(" -  Delete People " + (result_notifications.get(1) ? "Passed" : "Failed"));
+			
+			//Testing Transfer
+			result_transfer = moduleTesting.testTransfers();
+			
+			System.out.println(" - Transfering Tokens To User (Direct) " + (result_transfer.get(0) ? "Passed" : "Failed"));
+			System.out.println(" - Transfering Tokens To User (Peer Reviewed) " + (result_transfer.get(1) ? "Passed" : "Failed"));
+			
+			result_profile = moduleTesting.testProfile();
+			
+			System.out.println(" - Editing Profile " + (result_profile.get(0) ? "Passed" : "Failed"));
 			
 		}catch(Exception exception) {
 			System.out.println("Failed @ ModuleTesting() : " + exception);
