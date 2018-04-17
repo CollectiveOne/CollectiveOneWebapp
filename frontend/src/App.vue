4f2ffceeaf524b9c5a79f7c7b5b5b46d795fbe17
Pepo <pepo.ospina@gmail.com>
2018-04-17 13:26:15 +0200

    Updated Auth0Lock version Removed state in auth0 request (it was used for redirecting based on current url when trying to log in)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+  <template>
   <div class="container-fluid">
     <router-view></router-view>
   </div>
@@ -21,7 +21,6 @@ export default {
   created () {
     var options = {
       auth: {
-        state: this.$route.path,
         responseType: 'token',
         params: {
           connectionScopes: {
@@ -41,16 +40,12 @@ export default {
     var lock = new Auth0Lock(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN, options)
 
     lock.on('authenticated', (authResult) => {
-      console.log(authResult.accessToken)
-      console.log(authResult.idToken)
-      console.log(authResult.state)
       // authResult.accessToken = '6x5aVjAiyOJpe-Jhy5gVrnzp9eDh6AY9'
       // authResult.idToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2NvbGxlY3RpdmVvbmUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU5ZWRhY2RiYzMwYTM4MDUzYWI1YmMwZCIsImF1ZCI6Imt1RFgxWlZvckFseTVQWWR5VjcyMXpSb1RmMEswb3JtIiwiaWF0IjoxNTIwNDg5MjgwLCJleHAiOjE1MjMwODEyODB9.Xa75mfxW07TcYDfkofIPAhhCq3_wqVOss-M1rzF53Lw'
       // authResult.state = '/app/inits/'
       localStorage.setItem('access_token', authResult.accessToken)
       localStorage.setItem('id_token', authResult.idToken)
       this.$store.commit('authenticate', !!localStorage.getItem('id_token'))
-      this.$store.commit('setAuth0state', authResult.state)
       this.$store.dispatch('updateProfile')
       this.$router.push({ name: 'Root' })
     })
