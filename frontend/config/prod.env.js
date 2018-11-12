7593fd25ceca01d6b47565b6f6b2bca3795a073d
Pepo <pepo.ospina@gmail.com>
2018-11-12 16:10:38 +0100

    updated websocekt url

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   AUTH0_CLIENT_ID: '"kuDX1ZVorAly5PYdyV721zRoTf0K0orm"',
   AUTH0_DOMAIN: '"collectiveone.auth0.com"',
-  WEBSOCKET_SERVER_URL: '"https://collectiveone-dev.herokuapp.com"'
+  WEBSOCKET_SERVER_URL: '"https://collectiveone-v2.herokuapp.com/"'
 }
