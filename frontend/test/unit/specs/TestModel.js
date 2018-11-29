397cc8d8635db3b3b2a20edee3dcacf11833ecd8
Pepo <pepo.ospina@gmail.com>
2018-11-29 13:24:07 +0100

    trying to compile

--- frontend/test/unit/specs/TestModel.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import ModelSectionModal from '@/components/model/modals/ModelSectionModal.vue'
-
-describe('ModelSectionModal', () => {
-  it('add a detached copy of an existing section', () => {
-    console.dir('here')
-  })
-})
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  pwa: {
-    name: 'CollectiveOne',
-    themeColor: '#eff3f6',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {}
-  },
-
   devServer: {
     proxy: {
       '/1': {
