7b53e7caa260b3634b33b4fe0175de9f9c51afef
Pepo <pepo.ospina@gmail.com>
2018-11-30 15:17:13 +0100

    removed sw registration conditional

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -3,30 +3,29 @@ import { register } from 'register-service-worker'
 
 var swRegistration
 
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready (registration) {
-      swRegistration = registration
+register(`${process.env.BASE_URL}service-worker.js`, {
+  ready (registration) {
+    swRegistration = registration
 
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updated () {
-      console.log('New content is available; please refresh.')
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
-}
+    console.log(
+      'App is being served from cache by a service worker.\n' +
+      'For more details, visit https://goo.gl/AFskqB'
+    )
+  },
+  cached () {
+    console.log('Content has been cached for offline use.')
+  },
+  updated () {
+    console.log('New content is available; please refresh.')
+  },
+  offline () {
+    console.log('No internet connection found. App is running in offline mode.')
+  },
+  error (error) {
+    console.error('Error during service worker registration:', error)
+  }
+})
+
 
 export {
   swRegistration
