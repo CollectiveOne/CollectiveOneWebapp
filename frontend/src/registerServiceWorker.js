4569613018bbc1c72095756cc97ed44a97071dd0
Pepo <pepo.ospina@gmail.com>
2018-11-30 15:50:52 +0100

    added swRegistration update on hooks

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -6,16 +6,21 @@ var swRegistration
 register(`${process.env.BASE_URL}service-worker.js`, {
   ready (registration) {
     swRegistration = registration
-
     console.log(
       'App is being served from cache by a service worker.\n' +
       'For more details, visit https://goo.gl/AFskqB'
     )
   },
-  cached () {
+  registered (registration) {
+    swRegistration = registration
+    console.log('SW Registered')
+  },
+  cached (registration) {
+    swRegistration = registration
     console.log('Content has been cached for offline use.')
   },
-  updated () {
+  updated (registration) {
+    swRegistration = registration
     console.log('New content is available; please refresh.')
   },
   offline () {
