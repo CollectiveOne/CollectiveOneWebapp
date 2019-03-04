8a1cc2b8c0f16c87cd52cd54b115ed594c938b79
Pepo <pepo.ospina@gmail.com>
2019-03-04 18:05:37 +0100

    re-enable CORS

--- backend/src/main/java/org/collectiveone/config/AppConfig.java
+++ backend/src/main/java/org/collectiveone/config/AppConfig.java
@@ -51,16 +51,16 @@ public class AppConfig {
 		return pushService;		
 	}
 	
-//	@Bean
-//	public CorsFilter corsFilter() {
-//	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//	    final CorsConfiguration config = new CorsConfiguration();
-//	    config.setAllowCredentials(true);
-//	    config.setAllowedOrigins(Collections.singletonList("*"));
-//	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
-//	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-//	    source.registerCorsConfiguration("/**", config);
-//	    return new CorsFilter(source);
-//	}
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.setAllowedOrigins(Collections.singletonList("*"));
+	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
 	
 }
